0583f2a2785e2af6980fa91ef98aa35f56350099
Kento Okura <kentookura@gmail.com>
2025-03-28 15:12:11 +0100

    restore workspace symbol functionality

--- lib/language_server/Workspace_symbols.ml
+++ lib/language_server/Workspace_symbols.ml
@@ -5,156 +5,149 @@
  *
  *)
 
-(* open Forester_core *)
 open Forester_frontend
+open Forester_core
 open Forester_compiler
 
-(* module State = Analysis.State *)
 module L = Lsp.Types
 module Unit_map = Forester_compiler.Expand.Unit_map
+
+let (let*) = Option.bind
+
+let location_of_range loc =
+  match Option.map Range.view loc with
+  | None -> None
+  | Some`End_of_file {source; _}
+  | Some`Range ({source; _}, _) ->
+    match source with
+    | `String _ -> None
+    | `File path ->
+      if path = "" then
+        None
+      else
+        let uri = (Lsp.Uri.of_path path) in
+        Some
+          L.Location.{
+            range = Lsp_shims.Loc.lsp_range_of_range loc;
+            uri;
+          }
+
+let exports_to_symbols (exports : Tree.exports) =
+  Trie.to_seq exports
+  |> List.of_seq
+  |> List.filter_map (fun (path, (data, loc)) ->
+      match location_of_range loc with
+      | None -> None
+      | Some location ->
+        match data with
+        | Resolver.P.Xmlns _ ->
+          Some
+            (
+              L.SymbolInformation.create
+                ~kind: Namespace
+                ~location
+                ~name: (Format.asprintf "%a" Resolver.Scope.pp_path path)
+                ()
+            )
+        | Resolver.P.Term syn ->
+          let kind =
+            match (List.hd syn).value with
+            | Syn.Text _ -> L.SymbolKind.String
+            | Syn.Verbatim _ -> String
+            | Syn.Fun (_, _) -> Function
+            | Syn.Object _ -> Object
+            | Syn.Group (_, _)
+            | Syn.Math (_, _)
+            | Syn.Link _
+            | Syn.Subtree (_, _)
+            | Syn.Var _
+            | Syn.Sym _
+            | Syn.Put (_, _, _)
+            | Syn.Default (_, _, _)
+            | Syn.Get _
+            | Syn.Xml_tag (_, _, _)
+            | Syn.TeX_cs _
+            | Syn.Prim _
+            | Syn.Patch _
+            | Syn.Call (_, _)
+            | Syn.Results_of_query
+            | Syn.Transclude
+            | Syn.Embed_tex
+            | Syn.Ref
+            | Syn.Title
+            | Syn.Parent
+            | Syn.Taxon
+            | Syn.Meta
+            | Syn.Attribution (_, _)
+            | Syn.Tag _
+            | Syn.Date
+            | Syn.Number
+            | Syn.Dx_sequent (_, _)
+            | Syn.Dx_query (_, _, _)
+            | Syn.Dx_prop (_, _)
+            | Syn.Dx_var _
+            | Syn.Dx_const (_, _)
+            | Syn.Dx_execute
+            | Syn.Route_asset
+            | Syn.Publish_results_of_query
+            | Syn.Current_tree ->
+              Constant
+          in
+          Some
+            (
+              L.SymbolInformation.create
+                ~kind
+                ~location
+                ~name: (Format.asprintf "%a" Resolver.Scope.pp_path path)
+                ()
+            )
+    )
 
 let compute
     ({query = _; _}: L.WorkspaceSymbolParams.t)
   =
   let Lsp_state.{forest; _} = Lsp_state.get () in
-  let _render =
+  let render =
     Plain_text_client.string_of_content
       ~forest
       ~router: (Legacy_xml_client.route forest)
   in
-  let trees
+  let symbols
     =
     forest
     |> State.to_seq
-    |> Seq.filter_map
-        (fun (_uri, _item) -> None)
-    (* let title = *)
-    (*   match Forest.get_article uri forest.resources with *)
-    (*   | None -> "untitled" *)
-    (*   | Some {frontmatter; _} -> *)
-    (*     begin *)
-    (*       match frontmatter.title with *)
-    (*       | None -> "untitled" *)
-    (*       | Some content -> render content *)
-    (*     end *)
-    (* in *)
-    (* let uri = *)
-    (*   let* tree = Forest.find_opt forest.parsed uri in *)
-    (*   let* source_path = tree.source_path in *)
-    (*   Some (Lsp.Uri.of_path source_path) *)
-    (* in *)
-    (* match uri with *)
-    (* | None -> None *)
-    (* | Some uri -> *)
-    (*   let location = *)
-    (*     L.Location.{ *)
-    (*       range = L.Range.{ *)
-    (*         end_ = {character = 0; line = 0;}; *)
-    (*         start = {character = 0; line = 0;}; *)
-    (*       }; *)
-    (*       uri; *)
-    (*     } *)
-    (*   in *)
-    (*   Some (L.SymbolInformation.create ~kind: File ~location ~name: title ()) *)
     |> List.of_seq
+    |> List.concat_map
+        (fun (_, item) ->
+          let title =
+            match Tree.to_article item with
+            | Some {frontmatter; _} ->
+              render (State.get_expanded_title frontmatter forest)
+            | None -> "untitled"
+          in
+          let units =
+            List.concat @@
+            Option.to_list @@
+            Option.map exports_to_symbols (Tree.get_units item)
+          in
+          let lsp_uri = Option.map Lsp.Text_document.documentUri (Tree.to_doc item) in
+          let file_symbol =
+            Option.to_list @@
+              match lsp_uri with
+              | None -> None
+              | Some uri ->
+                let location =
+                  L.Location.{
+                    range = L.Range.{
+                      end_ = {character = 0; line = 0;};
+                      start = {character = 0; line = 0;};
+                    };
+                    uri;
+                  }
+                in
+                Some (L.SymbolInformation.create ~kind: File ~location ~name: title ())
+          in
+          units @ file_symbol
+        )
   in
-  (* let symbols = *)
-  (*   let open Forester_compiler in *)
-  (*   forest.units *)
-  (*   |> Unit_map.to_seq *)
-  (*   |> Seq.concat_map *)
-  (*       (fun ((uri, exports): URI.t * _) -> *)
-  (*         exports *)
-  (*         |> Trie.to_seq *)
-  (*         |> Seq.filter_map *)
-  (*             (fun (path, data) -> *)
-  (*               let location = *)
-  (*                 let range = *)
-  (*                   snd data *)
-  (*                   |> Lsp_shims.Loc.lsp_range_of_range *)
-  (*                 in *)
-  (*                 let uri = *)
-  (*                   let* tree = Forest.find_opt forest.parsed uri in *)
-  (*                   let* source_path = tree.source_path in *)
-  (*                   Some (Lsp.Uri.of_path source_path) *)
-  (*                 in *)
-  (*                 match uri with *)
-  (*                 | Some uri -> *)
-  (*                   Some (L.Location.{range; uri}) *)
-  (*                 | None -> None *)
-  (*               in *)
-  (*               let kind = *)
-  (*                 match data with *)
-  (*                 | (Resolver.P.Xmlns _, _loc) -> *)
-  (*                   L.SymbolKind.File *)
-  (*                 | (Resolver.P.Term syn, _loc) -> *)
-  (*                   match (List.hd syn).value with *)
-  (*                   | Syn.Fun (_, _) -> Function *)
-  (*                   | Syn.Text _ -> String *)
-  (*                   | Syn.Verbatim _ -> String *)
-  (*                   | Syn.Var _ -> Variable *)
-  (*                   | Syn.Title *)
-  (*                   | Syn.Taxon *)
-  (*                   | Syn.Meta *)
-  (*                   | Syn.Attribution (_, _) *)
-  (*                   | Syn.Tag _ *)
-  (*                   | Syn.Date *)
-  (*                   | Syn.Number -> *)
-  (*                     Field *)
-  (*                   | Syn.Object _ -> Object *)
-  (*                   | Syn.Group (_, _) *)
-  (*                   | Syn.Math (_, _) *)
-  (*                   | Syn.Link _ *)
-  (*                   | Syn.Subtree (_, _) *)
-  (*                   | Syn.Sym _ *)
-  (*                   | Syn.Put (_, _, _) *)
-  (*                   | Syn.Default (_, _, _) *)
-  (*                   | Syn.Get _ *)
-  (*                   | Syn.Xml_tag (_, _, _) *)
-  (*                   | Syn.TeX_cs _ *)
-  (*                   | Syn.Prim _ *)
-  (*                   | Syn.Patch _ *)
-  (*                   | Syn.Call (_, _) *)
-  (*                   | Syn.Query_polarity _ *)
-  (*                   | Syn.Query_mode _ *)
-  (*                   | Syn.Results_of_query *)
-  (*                   | Syn.Query_rel _ *)
-  (*                   | Syn.Query_isect *)
-  (*                   | Syn.Query_union *)
-  (*                   | Syn.Query_compl *)
-  (*                   | Syn.Query_isect_fam *)
-  (*                   | Syn.Query_union_fam *)
-  (*                   | Syn.Query_isect_fam_rel *)
-  (*                   | Syn.Query_union_fam_rel *)
-  (*                   | Syn.Query_builtin (_, _) *)
-  (*                   | Syn.Transclude *)
-  (*                   | Syn.Embed_tex *)
-  (*                   | Syn.Ref *)
-  (*                   | Syn.Parent *)
-  (*                   | Syn.Dx_sequent (_, _) *)
-  (*                   | Syn.Dx_query (_, _, _) *)
-  (*                   | Syn.Dx_prop (_, _) *)
-  (*                   | Syn.Dx_var _ *)
-  (*                   | Syn.Dx_const (_, _) *)
-  (*                   | Syn.Dx_execute *)
-  (*                   | Syn.Route_asset *)
-  (*                   | Syn.Publish_results_of_query *)
-  (*                   | Syn.Current_tree -> *)
-  (*                     L.SymbolKind.File *)
-  (*               in *)
-  (*               match location with *)
-  (*               | None -> None *)
-  (*               | Some location -> *)
-  (*                 Some *)
-  (*                   ( *)
-  (*                     L.SymbolInformation.create *)
-  (*                       ~kind *)
-  (*                       ~location *)
-  (*                       ~name: (Format.asprintf "%a" Resolver.Scope.pp_path path) *)
-  (*                       () *)
-  (*                   ) *)
-  (*             ) *)
-  (*       ) *)
-  (*   |> List.of_seq *)
-  (* in *)
-  Some (trees)
+  Some symbols
