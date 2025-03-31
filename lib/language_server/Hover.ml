56a4deaec3fe637e4da95f715f8c3ff1a762b93f
Kento Okura <kentookura@gmail.com>
2025-03-31 11:34:31 +0200

    improve hover handler

--- lib/language_server/Hover.ml
+++ lib/language_server/Hover.ml
@@ -8,21 +8,15 @@
 open Forester_core
 open Forester_compiler
 open Forester_frontend
+open Forester_search
+open State.Syntax
 
 module L = Lsp.Types
-(* module F = Analysis.F *)
-(* module PT = Analysis.PT *)
 module T = Types
 
-let compute
-    ({position;
-      textDocument;
-      _
-    }:
-      L.HoverParams.t
-    )
-    : L.Hover.t option
-  =
+let (let*) = Option.bind
+
+let compute ({position; textDocument; _}: L.HoverParams.t) =
   let Lsp_state.{forest; _} = Lsp_state.get () in
   let render =
     Plain_text_client.string_of_content
@@ -31,24 +25,29 @@ let compute
   in
   let config = forest.config in
   let host = config.host in
-  let content =
-    match State.get_code
-      forest
-      (URI_scheme.lsp_uri_to_uri ~host: forest.config.host textDocument.uri) with
-    | None -> "code of current tree is not stored. this is a bug"
+  let uri = URI_scheme.lsp_uri_to_uri ~host: forest.config.host textDocument.uri in
+  let* content =
+    match forest.={uri} with
+    | None -> Reporter.fatal Internal_error ~extra_remarks: [Asai.Diagnostic.loctextf "%a is not in the index" URI.pp uri]
     | Some tree ->
-      (* TODO: use node_at and provide hover for things other than links.*)
-      match Analysis.addr_at ~position tree.nodes with
-      | None -> Format.asprintf "character: %i, line: %i." position.character position.line;
-      | Some Range.{value = addr_at_cursor; _} ->
-        let uri_under_cursor = URI_scheme.user_uri ~host addr_at_cursor in
-        match State.get_article uri_under_cursor forest with
-        | None ->
-          Format.asprintf "Could not get article %a." URI.pp uri_under_cursor
-        | Some {mainmatter; frontmatter; _} ->
-          let main = render mainmatter in
-          if main = "" then (Format.asprintf "%a" T.(pp_frontmatter pp_content) frontmatter)
-          else main
+      match Tree.to_code tree with
+      | None -> None
+      | Some {nodes; _} ->
+        match Analysis.node_at ~position nodes with
+        | None -> None
+        | Some node ->
+          let tree_under_cursor =
+            let* {value = addr; _} = Analysis.extract_addr node in
+            let uri_under_cursor = URI_scheme.user_uri ~host addr in
+            State.get_article uri_under_cursor forest
+          in
+          match tree_under_cursor with
+          | Some article -> Some (render article.mainmatter)
+          | None ->
+            let* doc = Tree.to_doc tree in
+            let* search_term = Analysis.word_at ~position doc in
+            let results = List.map snd @@ Index.search forest.search_index search_term in
+            Some Format.(asprintf "Relevant results:@.%a@." (pp_print_list ~pp_sep: (fun out () -> fprintf out "@.") URI.pp) results)
   in
   Some
     (
