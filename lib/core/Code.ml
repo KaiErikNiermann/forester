0ffaed0aba8944d3038776f93dca54d4ee0c458c
Kento Okura <kentookura@gmail.com>
2025-04-01 15:32:39 +0200

    Add Code.map

--- lib/core/Code.ml
+++ lib/core/Code.ml
@@ -74,3 +74,38 @@ let display_math e = Math (Display, e)
 let parens e = Group (Parens, e)
 let squares e = Group (Squares, e)
 let braces e = Group (Braces, e)
+
+let map f node =
+  match node with
+  | Group (d, t) -> Group (d, f t)
+  | Math (m, t) -> Math (m, f t)
+  | Namespace (p, t) -> Namespace (p, f t)
+  | Dx_const_content t -> Dx_const_content (f t)
+  | Dx_const_uri t -> Dx_const_uri (f t)
+  | Def (p, b, t) -> Def (p, b, f t)
+  | Dx_sequent (t, ts) -> Dx_sequent (f t, List.map f ts)
+  | Dx_query (s, ts, rs) -> Dx_query (s, List.map f ts, List.map f @@ rs)
+  | Dx_prop (t, ts) -> Dx_prop (t, ts)
+  | Subtree (s, t) -> Subtree (s, f t)
+  | Let (p, b, t) -> Let (p, b, f t)
+  | Default (p, t) -> Default (p, f t)
+  | Scope t -> Scope (f t)
+  | Put (p, t) -> Put (p, f t)
+  | Fun (b, t) -> Fun (b, f t)
+  | Call (t, s) -> Call (f t, s)
+  | Object {self; methods} -> Object {self; methods = List.map (fun (s, t) -> (s, f t)) methods}
+  | Patch {obj; self; methods} -> Patch {obj = f obj; self; methods = List.map (fun (s, t) -> (s, f t)) methods}
+  | Text _
+  | Verbatim _
+  | Ident _
+  | Hash_ident _
+  | Xml_ident (_, _)
+  | Open _
+  | Get _
+  | Import (_, _)
+  | Decl_xmlns (_, _)
+  | Alloc _
+  | Dx_var _
+  | Comment _
+  | Error _ ->
+    node
