8823208f19427118a6db810280562338fc36ae23
Kento Okura <kentookura@gmail.com>
2025-04-12 15:18:50 +0200

    fix printing of Trie.path

--- lib/core/Trie.ml
+++ lib/core/Trie.ml
@@ -10,5 +10,5 @@ type path = string list
 [@@deriving repr]
 
 let pp_path =
-  let pp_sep fmt () = Format.pp_print_string fmt "." in
+  let pp_sep fmt () = Format.pp_print_string fmt "/" in
   Format.pp_print_list ~pp_sep Format.pp_print_string
