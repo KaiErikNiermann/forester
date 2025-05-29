c9e8a89bd09894890ea17306deb796df3e643907
Jon Sterling <jon@jonmsterling.com>
2025-05-29 13:50:11 +0100

    Reformat Suggestions.ml

--- lib/compiler/Suggestions.ml
+++ lib/compiler/Suggestions.ml
@@ -4,7 +4,9 @@
  * SPDX-License-Identifier: GPL-3.0-or-later OR Apache-2.0 WITH LLVM-exception
  *
  *)
- open Forester_core
+
+open Forester_prelude
+open Forester_core
 open Bwd
 
 (* TODO: remove this in favor of https://github.com/ocaml/ocaml/pull/13760 *)
@@ -12,10 +14,10 @@ let edit_distance ~cutoff x y =
   let len_x, len_y = String.length x, String.length y in
   let grid = Array.make_matrix (len_x + 1) (len_y + 1) 0 in
   for i = 1 to len_x do
-    grid.(i).(0) <- i;
+    grid.(i).(0) <- i
   done;
   for j = 1 to len_y do
-    grid.(0).(j) <- j;
+    grid.(0).(j) <- j
   done;
   for j = 1 to len_y do
     for i = 1 to len_x do
@@ -29,26 +31,16 @@ let edit_distance ~cutoff x y =
   else
     Some result
 
-let suggestions
-    ?prefix
-    ~(cutoff : int)
-    (p : Trie.bwd_path)
-    : ('data, 'tag) Trie.t -> ('data, int) Trie.t
-  =
+let suggestions ?prefix ~(cutoff : int) (p : Trie.bwd_path) : ('data, 'tag) Trie.t -> ('data, int) Trie.t =
   let compare p d =
-    edit_distance ~cutoff (String.concat "" (Bwd.to_list p)) (String.concat "" (Bwd.to_list d))
+    edit_distance
+      ~cutoff
+      (String.concat "" (Bwd.to_list p))
+      (String.concat "" (Bwd.to_list d))
   in
-  Trie.filter_map
-    ?prefix
-    (fun q (data, _) ->
-      match compare p q with
-      | Some i ->
-        if i > cutoff then
-          None
-        else
-          (Some (data, i))
-      | None -> None
-    )
+  Trie.filter_map ?prefix @@ fun q (data, _) ->
+  let@ i = Option.bind @@ compare p q in
+  if i > cutoff then None else Some (data, i)
 
 let suggestions ~visible path =
   suggestions ~cutoff: 2 (Bwd.of_list path) visible
