36c9f16a311d846be0dae409a313ff0324b8b822
Jon Sterling <jon@jonmsterling.com>
2025-08-19 14:03:03 +0100

    language server: correct computation of inlay hints
    1. Inlay hints computation should descend into children of nodes
    
    2. Inlay hints (e.g. for links) cannot be accurately computed on Code.t;
       we must instead wait for Syn.t.

--- lib/language_server/Inlay_hint.ml
+++ lib/language_server/Inlay_hint.ml
@@ -15,18 +15,44 @@ open struct
   module L = Lsp.Types
 end
 
+let list_of_option = function
+  | Some x -> [x]
+  | None -> []
+
+let consume_addr_for_inlay ~(config : Config.t) ~(forest : State.t) (node : Syn.node Range.located) : string option * Syn.t =
+  match node.value with
+  | Link {title = None; dest = [{value = Text addr; _}]} -> Some addr, []
+  | Subtree (addr, nodes) -> addr, nodes
+  | _ -> None, Syn.children node
+
+let inlay_hint_for_addr ~(config : Config.t) ~(forest : State.t) ~(pos : Range.position) (addr : string) : L.InlayHint.t option =
+  let uri = URI_scheme.named_uri ~base: config.url addr in
+  let@ {frontmatter; _} = Option.bind @@ State.get_article uri forest in
+  let@ title = Option.bind frontmatter.title in
+  let content = " " ^ Plain_text_client.string_of_content ~forest title in
+  Option.some @@ L.InlayHint.create ~position: (Lsp_shims.Loc.lsp_pos_of_pos pos) ~label: (`String content) ()
+
+let pos_of_node (node : 'a Range.located) : Range.position option =
+  let@ loc = Option.bind node.loc in
+  match Range.view loc with
+  | `End_of_file _ -> None
+  | `Range (_, pos) -> Some pos
+
+let rec extract_inlayable_hints ~(config : Config.t) ~(forest : State.t) (nodes : Syn.t) : L.InlayHint.t list =
+  let@ node = List.concat_map @~ nodes in
+  let addr_opt, rest = consume_addr_for_inlay ~config ~forest node in
+  let hint_opt =
+    let@ addr = Option.bind addr_opt in
+    let@ pos = Option.bind @@ pos_of_node node in
+    inlay_hint_for_addr ~config ~forest ~pos addr
+  in
+  let hints = extract_inlayable_hints ~config ~forest rest in
+  list_of_option hint_opt @ hints
+
+
 let compute (params : L.InlayHintParams.t) : L.InlayHint.t list option =
   let Lsp_state.{forest; _} = Lsp_state.get () in
   let config = forest.config in
   let uri = URI_scheme.lsp_uri_to_uri ~base: config.url params.textDocument.uri in
-  let@ {nodes; _} = Option.map @~ Option.bind forest.={uri} Tree.to_code in
-  let@ (Range.{loc; _} as node) = List.filter_map @~ Analysis.flatten nodes in
-  match Option.map Range.view loc with
-  | None | Some (`End_of_file _) -> None
-  | Some (`Range (_, pos)) ->
-    let@ {value = str; _} = Option.bind @@ Analysis.extract_addr node in
-    let uri = URI_scheme.named_uri ~base: config.url str in
-    let@ {frontmatter; _} = Option.bind @@ State.get_article uri forest in
-    let@ title = Option.map @~ frontmatter.title in
-    let content = " " ^ Plain_text_client.string_of_content ~forest title in
-    L.InlayHint.create ~position: (Lsp_shims.Loc.lsp_pos_of_pos pos) ~label: (`String content) ()
+  let@ {nodes; _} = Option.map @~ Option.bind forest.={uri} Tree.to_syn in
+  extract_inlayable_hints ~config ~forest nodes
